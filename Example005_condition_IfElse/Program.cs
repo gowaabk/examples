2bc2d0889ca39a0c5e6d031990d0bb6daa164b6a
Gowaa <gowaa@bk.ru>
2022-06-22 13:31:20 +0300

    Example 005 Condition IfElse

---
+++ Example005_condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow it's Masha");
+}
+else
+{
+    Console.Write("Приувет, ");
+    Console.WriteLine(username);
+}
